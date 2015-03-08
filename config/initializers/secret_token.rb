089ec5693ce7f546ce664fe2df9dd46a968476e8
umairamjad <umairamjadawan@gmail.com>
2015-03-08 20:44:46 +0500

    adding all project files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestApp::Application.config.secret_token = 'c95a2c04a6d69f327630c374354acd4b6abe7954e1c004841ebcd5e82fe7c8d5d5820b8734bfe74cd12b17467f6a6fe201df4ff9f073293ce9a51ad9fe7325e1'
---
+++ db/migrate/20150308061234_add_user_id_columns_to_posts.rb
@@ -0,0 +1,4 @@
+class AddUserIdColumnsToPosts < ActiveRecord::Migration
+  def change
+  end
+end
